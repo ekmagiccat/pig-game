732a0b9ed3a0cc868a5547b0d0da3dc23ef0e9a0
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-15 14:15:27 -0700

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,14 @@
 //business logic
 
-function Game() {
-  this.dieRoll = 0;
-  this.turnScore = 0;
-  this.totalScore = 0;
-  this.id;
+function Game(dieRoll, turnScore, totalScore, id) {
+  this.dieRoll = dieRoll;
+  this.turnScore = turnScore;
+  this.totalScore = totalScore;
+  this.id= id; //Do we need to define id as 'this.id = id;'?
 }
 
-/* function Player(){
-  this.player1 = player1;
-  this.player2 = player2;
-  this.currentPlayer = {};
-}; */
-
 Game.prototype.Roll = function () {
-  this.dieRoll = math.ceil(Math.random() * 6);
+  this.dieRoll = Math.ceil(Math.random() * 6);
 };
 
 Game.prototype.rollTurn = function () {
@@ -22,9 +16,9 @@ Game.prototype.rollTurn = function () {
     this.turnScore = 0;
   } else {
     this.dieRoll > 1;
-    this.turnScore += this.totalScore = 0;
+    this.turnScore += this.dieRoll;
   }
-  return totalScore;
+  return this.turnScore;
 };
 
 Game.prototype.turnTotal = function () {
@@ -34,9 +28,7 @@ Game.prototype.turnTotal = function () {
 
 Game.prototype.firstTo100 = function () {
   if (totalScore >= 100) {
-    console.log(
-      "You are the winner wooooooooooooooooooooooooooooooooo1o1o1o1o1o11o1o1o"
-    );
+    console.log("You are the winner wooooooooooooooooooooooooooooooooo");
     return this.totalScore;
   }
 };
@@ -46,6 +38,7 @@ let playerTurn1 = new Game(0, 0, 0, 1);
 let playerTurn2 = new Game(0, 0, 0, 2);
 
 function playerScores(Game) {
+  console.log(Game);
   if (Game.id === 1) {
     let playerRoll1 = document.getElementById("playerRoll1");
     let player1TurnScore = document.getElementById("player1TurnScore");
@@ -55,23 +48,76 @@ function playerScores(Game) {
     player1TurnScore.innerText = Game.turnScore;
     player1TotalScore.innerText = Game.totalScore;
   }
+
+  if (Game.id === 2) {
+    let playerRoll2 = document.getElementById("playerRoll2");
+    let player2TurnScore = document.getElementById("player2TurnScore");
+    let player2TotalScore = document.getElementById("player2TotalScore");
+
+    playerRoll2.innerText = Game.dieRoll;
+    player2TurnScore.innerText = Game.turnScore;
+    player2TotalScore.innerText = Game.totalScore;
+  }
 }
 
-if (Game.id === 2) {
-  let playerRoll2 = document.getElementById("playerRoll2");
-  let player2TurnScore = document.getElementById("player2TurnScore");
-  let player2TotalScore = document.getElementById("player2TotalScore");
+function Play() {
+  if (event.target.id === "roll1") {
+    playerTurn1.Roll();
+    playerTurn1.rollTurn();
+    playerScores(playerTurn1);
+  } else if (event.target.id === "hold1") {
+    playerTurn1.turnTotal();
+    playerScores(playerTurn1);
+  } else if (event.target.id === "roll2") {
+    playerTurn2.Roll();
+    playerTurn2.rollTurn();
+    playerScores(playerTurn2);
+  } else if (event.target.id === "hold2") {
+    playerTurn2.turnTotal();
+    playerScores(playerTurn2);
+  }
+  return false;
 
-  playerRoll2.innerText = Game.dieRoll;
-  player2TurnScore.innerText = Game.turnScore;
-  player2TotalScore.innerText = Game.totalScore;
+  
 }
 
-let roll1 = document.getElementById("roll1");
-roll1.addEventListener("click", Game);
+window.onload = function () {
+  let roll1 = document.getElementById("roll1");
+  roll1.addEventListener("click", Play);
+
+  let hold1 = document.getElementById("hold1");
+  roll1.addEventListener("click", Play);
+
+  let roll2 = document.getElementById("roll2");
+  roll2.addEventListener("click", Play);
+
+  let hold2 = document.getElementById("roll2");
+  hold2.addEventListener("click", Play);
+};
+
+// //function diceWinner(Game) {
+/* "Dice Player" + Game.id + "is the Winner!";
+/* }
+
+//potential button listeners
+let rollButton1 = document.getElementById("roll1");
+rollButton1.addEventListener("click", function () {
+  playerTurn1.Roll();
+  playerScores(playerTurn1);
+});
+
+let rollButton2 = document.getElementById("roll2");
+rollButton2.addEventListener("click", function () {
+  playerTurn2.Roll();
+  playerScores(playerTurn2);
+});
+
+//previous code that didn't work:
+ let roll1 = document.getElementById("roll1");
+roll1.addEventListener("submit", Game.Roll);
 
 let roll2 = document.getElementById("roll2");
-roll2.addEventListener("click", Game);
+roll2.addEventListener("submit", Game.Roll); */
 
 /* window.addEventListener("load", function () {
   document
