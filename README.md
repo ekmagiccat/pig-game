f2f6785e2be6bfe01d822d170cdf8f186d491a62
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-15 10:14:29 -0700

    complete business logic for js

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 Describe: function game();
 
-Test: "It should create base score for maxScore and dieRoll. "
-Input: game()
-Expected output:
+Test: "It should create base score for maxScore, dieRoll, totalScore and currentScore. "
+Input: game(10)
+Expected output:game(10)
+
+
 
 Describe: function(player);
 
 Test: "It should define Player 1 and Player 2 and currentPlay."
 Input: player(player1, player2)
-Expected output: [player1, player2]
+Expected output: [player1, player2, currentPlayer]
 
-Describe:
+Describe: Game.prototype.Roll function ()
 
 Test:
 Input:
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,48 @@
 //business logic
 
-function game(){
-  this.maxScore = 0;
+function Game() {
   this.dieRoll = 0;
-}
+  this.maxScore = 0;
+  this.turnScore = 0;
+  this.totalScore = 0;
+};
 
-function player(){
+/* function Player(){
   this.player1 = player1;
   this.player2 = player2;
   this.currentPlayer = {};
-}
+}; */
+
+
+Game.prototype.Roll = function () {
+  this.dieRoll = math.ceil(Math.random() * 6);
+};
+
+Game.prototype.rollTurn = function () {
+  if (this.dieRoll === 1) {
+    this.turnScore === 0;
+  } else {
+    this.dieRoll > 1;
+    this.turnScore + this.totalScore === 0;
+  }
+  return totalScore;
+};
+
+Game.prototype.turnTotal = function () {
+  this.totalScore += this.turnScore;
+  this.turnScore = 0;
+};
+
+Game.prototype.firstTo100 = function () {
+  if (maxScore >= 100) {
+    console.log(
+      "You are the winner wooooooooooooooooooooooooooooooooo1o1o1o1o1o11o1o1o"
+    );
+    return this.maxScore;
+  }
+};
+
+//ui logic
 
 
 
@@ -18,12 +51,9 @@ function player(){
 
 
 
-
-
-
-
-function getRandomNum(min, max) {
+/*function getRandomNum(min, max) {
   min = Math.ceil(1);
   max = Math.ceil(6);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+*/
