1b963d29111961d731f8304bd569f7205cbdd56f
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-15 11:48:50 -0700

    add coauthors
    Co-authored-by: Pier Rodriguez <pierknows@gmail.com>
    
    Co-authored-by: Richard Lemmond <rglemmond86@yahoo.com>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Describe: function game();
 
-Test: "It should create base score for maxScore, dieRoll, totalScore and currentScore. "
+Test: "It should create base score for dieRoll, totalScore and currentScore. "
 Input: game(10)
 Expected output:game(10)
 
@@ -17,3 +17,29 @@ Describe: Game.prototype.Roll function ()
 Test:
 Input:
 Expected output:
+
+
+Describe: Game.prototype.rollTurn = function ()
+
+Test:
+Input:
+Expected output:
+
+Describe: Game.prototype.turnTotal = function
+
+Test: "It should add the current player's round score to their global score, and then set the round score back to 0."
+Input: game.turnTotal();
+Expected output: The global score of Player1 is x and the round score is 0.
+
+Describe: Game.prototype.firstTo100 = function
+
+Test: "It should anounce winner when score has reached 100."
+Input: (maxScore >= 100)
+Expected output: console.log( "You are the winner oooooooooooooooooooooooooooooooooo1o1o1o1o1o11o1o1o")
+
+Describe: 
+
+Test:
+Input:
+Expected output:
+
--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@
 
 function Game() {
   this.dieRoll = 0;
-  this.maxScore = 0;
   this.turnScore = 0;
   this.totalScore = 0;
-};
+  this.id;
+}
 
 /* function Player(){
   this.player1 = player1;
@@ -13,17 +13,16 @@ function Game() {
   this.currentPlayer = {};
 }; */
 
-
 Game.prototype.Roll = function () {
   this.dieRoll = math.ceil(Math.random() * 6);
 };
 
 Game.prototype.rollTurn = function () {
   if (this.dieRoll === 1) {
-    this.turnScore === 0;
+    this.turnScore = 0;
   } else {
     this.dieRoll > 1;
-    this.turnScore + this.totalScore === 0;
+    this.turnScore += this.totalScore = 0;
   }
   return totalScore;
 };
@@ -34,22 +33,81 @@ Game.prototype.turnTotal = function () {
 };
 
 Game.prototype.firstTo100 = function () {
-  if (maxScore >= 100) {
+  if (totalScore >= 100) {
     console.log(
       "You are the winner wooooooooooooooooooooooooooooooooo1o1o1o1o1o11o1o1o"
     );
-    return this.maxScore;
+    return this.totalScore;
   }
 };
 
 //ui logic
+let playerTurn1 = new Game(0, 0, 0, 1);
+let playerTurn2 = new Game(0, 0, 0, 2);
 
+function playerScores(Game) {
+  if (Game.id === 1) {
+    let playerRoll1 = document.getElementById("playerRoll1");
+    let player1TurnScore = document.getElementById("player1TurnScore");
+    let player1TotalScore = document.getElementById("player1TotalScore");
 
+    playerRoll1.innerText = Game.dieRoll;
+    player1TurnScore.innerText = Game.turnScore;
+    player1TotalScore.innerText = Game.totalScore;
+  }
+}
 
+if (Game.id === 2) {
+  let playerRoll2 = document.getElementById("playerRoll2");
+  let player2TurnScore = document.getElementById("player2TurnScore");
+  let player2TotalScore = document.getElementById("player2TotalScore");
 
+  playerRoll2.innerText = Game.dieRoll;
+  player2TurnScore.innerText = Game.turnScore;
+  player2TotalScore.innerText = Game.totalScore;
+}
 
+let roll1 = document.getElementById("roll1");
+roll1.addEventListener("click", Game);
 
+let roll2 = document.getElementById("roll2");
+roll2.addEventListener("click", Game);
 
+/* window.addEventListener("load", function () {
+  document
+    .querySelector("form#bankAccount")
+    .addEventListener("submit", handleNewBankAccount);
+  document
+    .querySelector("form#depositWithdrawal")
+    .addEventListener("submit", handleDepositWithdrawals);
+}); */
+
+// let player2 = document.querySelector("input[id='player-2']:checked").value;
+
+/* - Richard's Confused Corner - 
+
+Game.prototype.rollTurn = function () {
+  if (this.dieRoll === 1) {
+    this.turnScore = 0;
+  } else {
+    this.dieRoll > 1; <- is this redundant?
+    this.turnScore += this.totalScore = 0; <- combines then totals at 0?
+  }
+  return totalScore;
+}; 
+
+Would this work better?
+Game.prototype.rollTurn = function () {
+  if (this.dieRoll === 1) {
+    this.turnScore = 0;
+  } else {
+    this.turnScore += this.dieRoll;
+    this.totalScore += this.turnScore;
+    this.turnScore = 0;
+  }
+  return this.totalScore;
+};
+*/
 
 /*function getRandomNum(min, max) {
   min = Math.ceil(1);
